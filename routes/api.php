9af3075b1f2004869b88efe2cb4c2929ea595537
Дмитрий <markelovdn@gmail.com>
2024-02-20 22:14:31 +0300

    test autodeploy

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,10 @@ Route::get('/getUserByToken', function () {
     return json_encode(['message' => 'Hello World!']);
 });
 
+Route::get('/test', function () {
+    return json_encode(['message' => 'test!']);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
