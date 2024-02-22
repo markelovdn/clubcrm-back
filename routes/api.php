97b390b03f155141a41f7bc4894f1707e0d8dd5a
Дмитрий <markelovdn@gmail.com>
2024-02-22 21:48:03 +0300

    auth

--- routes/api.php
+++ routes/api.php
@@ -31,7 +31,7 @@ Route::get('/handleVKCallback', function (Request $request) {
                 'fields'       => 'email', // Запрашиваем email
             );
             $info = file_get_contents('https://api.vk.com/method/users.get?' . urldecode(http_build_query($params)));
-            return json_encode($info);
+            return json_decode($info, true);
         }
     }
 });
