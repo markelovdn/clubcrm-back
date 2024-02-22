ade7f11842fee9b01f77f4ec9a52d937eca25d12
Дмитрий <markelovdn@gmail.com>
2024-02-22 08:11:21 +0300

    vk auth

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,33 @@ Route::get('/test', function () {
 
 Route::get('/authFromVK', [AuthController::class, 'authFromVK']);
 Route::get('/handleVKCallback', function (Request $request) {
-    return $request->all();
+    if (!empty($_GET['code'])) {
+        $params = array(
+            'client_id'     => '51858715',
+            'client_secret' => 'N5VU9q4KhRPwosHYVeEw',
+            'redirect_uri'  => 'https://legion34.clubcrm.ru/api/handleVKCallback',
+            'code'          => $_GET['code']
+        );
+
+        // Получение access_token
+        $data = file_get_contents('https://oauth.vk.com/access_token?' . urldecode(http_build_query($params)));
+        $data = json_decode($data, true);
+        if (!empty($data['access_token'])) {
+            // Получили email
+            $email = $data['email'];
+
+            // Получим данные пользователя
+            $params = array(
+                'v'            => '5.52',
+                'uids'         => $data['user_id'],
+                'access_token' => $data['access_token'],
+                'fields'       => 'photo_big',
+            );
+
+            $info = file_get_contents('https://api.vk.com/method/users.get?' . urldecode(http_build_query($params)));
+            return json_decode($info, true);
+        }
+    }
 });
 
 
