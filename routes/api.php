3a891ea3dd68e20690c4ee51425db6463a96d673
Дмитрий <markelovdn@gmail.com>
2024-02-24 11:40:30 +0300

    auth

--- routes/api.php
+++ routes/api.php
@@ -30,8 +30,7 @@ Route::get('/handleVKCallback', function (Request $request) {
                 'access_token' => $data['access_token'],
                 'fields'       => 'email', // Запрашиваем email
             );
-            $info = file_get_contents('https://api.vk.com/method/account.getProfileInfo
-            ?' . urldecode(http_build_query($params)));
+            $info = file_get_contents('https://api.vk.com/method/account.getProfileInfo?' . urldecode(http_build_query($params)));
             return json_decode($info, true);
         }
     }
