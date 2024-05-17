26c9a77cdd574810cd4d006227b1dde71d9e91eb
Дмитрий <markelovdn@gmail.com>
2024-05-17 17:43:09 +0300

    доработал бота для отправки сообщений из тг канала

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -3,42 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Mail;
 
 class TelegramController extends Controller
 {
+    private $recipients = ['markelovdn@gmail.com', 'kintexrus@list.ru'];
+
     public function handle(Request $request)
     {
         $message = $request->input('message');
 
-        if (isset($message['text'])) {
-            $chatId = $message['chat']['id'];
-            $text = $message['text'];
+        if ($message && $message['chat']['type'] === 'channel') {
+            $text = $message['text'] ?? 'Текст сообщения отсутствует';
+            $channelTitle = $message['chat']['title'] ?? 'Неизвестный канал';
 
-            Mail::raw($text, function ($msg) {
-                $msg->to('markelovdn@gmail.com')->subject('Сообщение от СК Легион');
-            });
+            $subject = "Сообщение от канала: $channelTitle";
 
-            Mail::raw($text, function ($msg) {
-                $msg->to('kintexrus@list.ru')->subject('Сообщение от СК Легион');
-            });
+            foreach ($this->recipients as $recipient) {
+                Mail::raw($text, function ($msg) use ($recipient, $subject) {
+                    $msg->to($recipient)->subject($subject);
+                });
+            }
 
-            $responseText = 'Ваше сообщение было отправлено на email.';
-
-            $this->sendMessage($chatId, $responseText);
+            return response()->json(['status' => 'Message sent to email']);
         }
 
-        return response()->json(['status' => 'ok']);
-    }
-
-    private function sendMessage($chatId, $text)
-    {
-        $url = "https://api.telegram.org/bot" . config('services.telegram_bot_token') . "/sendMessage";
-
-        Http::post($url, [
-            'chat_id' => $chatId,
-            'text' => $text,
-        ]);
+        return response()->json(['status' => 'No valid message found'], 400);
     }
 }
