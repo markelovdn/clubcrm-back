aafad939a4d06a0b6e1c0f7e91c1ab05403ec714
Dmitrii Markelov <63663198+markelovdn@users.noreply.github.com>
2024-07-17 16:15:54 +0300

    Delete .dbprod directory

--- .dbprod/updb.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-aws --endpoint-url=https://storage.yandexcloud.net \s3 cp `date +s3://legion/legioncrm/db/backupdb_crmlegion_%Y-%m-%d`.00:00:01.sql.gz /var/www/clubcrm_back.loc/.dbprod/db.gz && gunzip /var/www/clubcrm_back.loc/.dbprod/db.gz && mysql -u clubcrm_loc -pDn100883! clubcrm_loc < /var/www/clubcrm_back.loc/.dbprod/db; rm db && cd .. && php artisan migrate && php artisan optimize
